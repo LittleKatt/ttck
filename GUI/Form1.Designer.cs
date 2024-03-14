ef1c98c0d92113c02c47b0360cdd9c6ee140785c
LittleKatt <0912327601pmt@gmail.com>
2024-03-14 18:15:38 +0700

    h

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -194,7 +194,7 @@
             this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
             this.ribbonPageGroup1});
             this.ribbonPage1.Name = "ribbonPage1";
-            this.ribbonPage1.Text = "Hệ thống 7";
+            this.ribbonPage1.Text = "Hệ thống";
             // 
             // ribbonPageGroup1
             // 
